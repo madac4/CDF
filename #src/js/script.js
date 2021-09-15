d3a18abe3972075728e28721287703c597dd4081
Fedotos228 <78444516+Fedotos228@users.noreply.github.com>
2021-09-15 19:21:29 +0300

    header full

---
+++ #src/js/script.js
@@ -0,0 +1,17 @@
+if (document.querySelector('.header')) {
+    const languageToggler = document.querySelector('.language')
+    const languageDropdown = document.querySelector('.language-dropdown')
+    const navLink = document.querySelectorAll('.with-subnav')
+    const subnavToggler = document.querySelector('.subnav')
+
+
+    navLink.forEach(el => {
+        el.addEventListener('click', () => {
+            subnavToggler.classList.toggle('open')
+        });
+    })
+
+    languageToggler.addEventListener('click', () => {
+        languageToggler.classList.toggle('open')
+    })
+}
